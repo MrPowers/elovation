9f093761e3fe774dd94edad7c88b6986c94e300a
Shawn Burns <sburns@medivo.com>
2014-07-17 17:13:33 -0400

    Upgrade Ruby version Add JSON to RatingsController

--- app/controllers/ratings_controller.rb
+++ app/controllers/ratings_controller.rb
@@ -1,5 +1,10 @@
 class RatingsController < ApplicationController
   def index
     @game = Game.find(params[:game_id])
+    
+    respond_to do |format|
+    	format.html
+		format.json { render json: @game }
+	end
   end
 end
